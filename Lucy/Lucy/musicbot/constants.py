ea735f5997090af57c9e5ab07003434f88b12cd7
CreeperSeth <creeperseth@mail.com>
2016-06-28 09:33:40 -0700

    RobTheBoat much?

---
+++ Lucy/Lucy/musicbot/constants.py
@@ -0,0 +1,12 @@
+import os.path
+
+VERSION = '2.8.4'
+MAIN_VERSION = '2'
+SUB_VERSION = '-rev 4 '
+CODENAME = '"Twenty One Pilots"' #I WANNA BE KNOWN BY YOUUU
+VER = VERSION + SUB_VERSION
+BDATE = 'June 27, 2016 @ 2:17 AM EDT'
+MAINVER = VERSION + SUB_VERSION + CODENAME
+BUILD_USERNAME = "Wyndrik and CreeperSeth"
+AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
+DISCORD_MSG_CHAR_LIMIT = 2000
