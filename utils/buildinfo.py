58b7199d86edf974e55a0928ee3a3cea76fdf8b6
CreeperSeth <CreeperSeth@users.noreply.github.com>
2017-08-31 22:21:32 -0700

    testing gpg key

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,4 +1,4 @@
 BUILD_VERSION = "14.1"
 BUILD_AUTHORS = "ZeroEpoch1969#0051 and based robin#0052"
 BUILD_CODENAME = "im edgy"
-BUILD_DATE = "August 19th, 2017 at 8:22 PM GMT -7:00"
+BUILD_DATE = "August 31st, 2017 at 10:21 PM GMT -7:00"
