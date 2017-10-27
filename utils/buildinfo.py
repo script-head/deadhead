19ea718f8ee32d6c293b2eb398a9b0c23f0bb466
CreeperSeth <13510767+ZeroEpoch1969@users.noreply.github.com>
2017-10-26 21:16:52 -0700

    DOES NOBODY KNOW IM INSANE?

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -1,5 +1,5 @@
 BUILD_VERSION = "15.0"
 BUILD_AUTHORS = "ZeroEpoch1969#0051 and spooked robin#0052"
 BUILD_CODENAME = "BEATEN DOWN, DOMINATED BY ITS SOUND"
-BUILD_DATE = "October 22nd, 2017 at 3:45 PM GMT -7:00"
+BUILD_DATE = "October 26th, 2017 at 9:16 PM GMT -7:00"
 TRANSLATORS = {"spooked robin#0052":"Spanish", "BibleRule#9942":"Hebrew", "Timanttikuutio#1964":"Finnish", "Ele_#0400":"Spanish", "Naratna#1674":"French"}
