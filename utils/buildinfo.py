2cf6b39c970131dcf5d7014af778fd4103fb82f4
Naratna <antar.espadas@hotmail.com>
2021-03-28 01:57:59 -0600

    Fixed outdated translator name

--- utils/buildinfo.py
+++ utils/buildinfo.py
@@ -2,4 +2,4 @@ BUILD_VERSION = "15.2"
 BUILD_AUTHORS = "scripthead#7988"
 BUILD_CODENAME = ":yeepensive:"
 BUILD_DATE = "October 23rd, 2020 at 6:36 PM GMT -7:00"
-TRANSLATORS = {"based robin#0052":"Spanish", "BibleRule#9942":"Hebrew", "Timanttikuutio#1964":"Finnish", "Ele_#0400":"Spanish", "Naratna#1674":"French"}
+TRANSLATORS = {"based robin#0052":"Spanish", "BibleRule#9942":"Hebrew", "Timanttikuutio#1964":"Finnish", "Ele_#6969":"Spanish", "Naratna#1674":"French"}
